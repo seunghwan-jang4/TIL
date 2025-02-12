f1807c6ffbf6d60edd1892e73b63c57ef7e8b8f4
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-12 23:27:29 +0900

    update: 최대힙 구현

--- sparta_algorithm/2nd_week/structures.py
+++ sparta_algorithm/2nd_week/structures.py
@@ -123,3 +123,77 @@ class HashTable:
                 return
             prev_node = node
             node = node.next
+
+
+# 최대 힙 구현 - 삽입
+class BinaryMaxHeap:
+    def __init__(self):
+        # 계산 편의를 위해 0이 아닌 1번째 인덱스부터 사용한다.
+        self.items = [None]
+
+    def insert(self, k):
+        self.items.append(k)
+        self._percolate_up()
+
+    def _percolate_up(self):
+        # percolate: 스며들다.
+        cur = len(self.items) - 1
+        # left 라면 2*cur, right 라면 2*cur + 1 이므로 parent 는 항상 cur // 2
+        parent = cur // 2
+
+        while parent > 0:
+            if self.items[cur] > self.items[parent]:
+                self.items[cur], self.items[parent] = self.items[parent], self.items[cur]
+
+            cur = parent
+            parent = cur // 2
+            
+
+# 최대 힙 구현 - 추출
+class BinaryMaxHeap:
+    def __init__(self):
+        # 계산 편의를 위해 0이 아닌 1번째 인덱스부터 사용한다.
+        self.items = [None]
+
+    def insert(self, k):
+        self.items.append(k)
+        self._percolate_up()
+
+    def _percolate_up(self):
+        # percolate: 스며들다.
+        cur = len(self.items) - 1
+        # left 라면 2*cur, right 라면 2*cur + 1 이므로 parent 는 항상 cur // 2
+        parent = cur // 2
+
+        while parent > 0:
+            if self.items[cur] > self.items[parent]:
+                self.items[cur], self.items[parent] = self.items[parent], self.items[cur]
+
+            cur = parent
+            parent = cur // 2
+
+    def extract(self):
+        if len(self.items) < 2: 
+            return None
+
+        root = self.items[1]
+        self.items[1] = self.items[-1]
+        self.items.pop()
+        self._percolate_down(1)
+
+        return root
+
+    def _percolate_down(self, cur):
+        biggest = cur
+        left = 2 * cur
+        right = 2 * cur + 1
+
+        if left <= len(self.items) - 1 and self.items[left] > self.items[biggest]:
+            biggest = left
+
+        if right <= len(self.items) - 1 and self.items[right] > self.items[biggest]:
+            biggest = right
+
+        if biggest != cur:
+            self.items[cur], self.items[biggest] = self.items[biggest], self.items[cur]
+            self._percolate_down(biggest)
