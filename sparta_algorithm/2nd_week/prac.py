6e508e4743e7ad6e8326e9ba7a56febc3fa92116
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-17 23:11:16 +0900

    update: 그래프 탐색, DFS

--- sparta_algorithm/2nd_week/prac.py
+++ sparta_algorithm/2nd_week/prac.py
@@ -51,3 +51,34 @@ def test_problem_queue(num):
         deq.popleft()
         deq.append(deq.popleft())
     return deq.popleft()
+
+
+# 그래프 탐색
+def dfs_recursive(graph, node, visited):
+    # 방문처리
+    visited.append(node)
+
+    # 인접 노드 방문
+    for adj in graph[node]:
+        if adj not in visited:
+            dfs_recursive(graph, adj, visited)
+
+    return visited
+
+
+def dfs_stack(graph, start):
+    visited = []
+    # 방문할 순서를 담아두는 용도
+    stack = [start]
+
+    # 방문할 노드가 남아있는 한 아래 로직을 반복한다.
+    while stack:
+        # 제일 최근에 삽입된 노드를 꺼내고 방문처리한다.
+        top = stack.pop()
+        visited.append(top)
+        # 인접 노드를 방문한다.
+        for adj in graph[top]:
+            if adj not in visited:
+                stack.append(adj)
+
+    return visited
