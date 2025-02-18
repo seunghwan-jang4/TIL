8c677f2466e88aaa1bbe55817b5765626e9dbd0d
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-18 22:03:06 +0900

    update: DFS 문제, BFS 문제

--- sparta_algorithm/3rd_week/prac.py
+++ sparta_algorithm/3rd_week/prac.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # 그래프 탐색
 def dfs_recursive(graph, node, visited):
     # 방문처리
@@ -27,3 +29,114 @@ def dfs_stack(graph, start):
                 stack.append(adj)
 
     return visited
+
+# 섬의 개수 (스택)
+def island_dfs_stack(grid):
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+    rows, cols = len(grid), len(grid[0])
+    cnt = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] != '1':
+                continue
+
+            cnt += 1
+            stack = [(row, col)]
+
+            while stack:
+                x, y = stack.pop()
+                grid[x][y] = '0'
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if nx < 0 or nx >= rows or ny < 0 or ny >= cols or grid[nx][ny] != '1':
+                        continue
+                    stack.append((nx, ny))
+    return cnt
+
+# 섬의 개수 (재귀)
+def island_dfs_recursive(grid):
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+    m = len(grid)
+    n = len(grid[0])
+    cnt = 0
+
+    def dfs_recursive(r, c):
+        if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] != '1':
+            return
+
+        # 방문처리
+        grid[r][c] = '0'
+        for i in range(4):
+            dfs_recursive(r + dx[i], c + dy[i])
+        return
+
+    for r in range(m):
+        for c in range(n):
+            node = grid[r][c]
+            if node != '1':
+                continue
+
+            cnt += 1
+            dfs_recursive(r, c)
+
+    return cnt
+
+assert island_dfs_recursive(grid=[
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+]) == 1
+assert island_dfs_recursive(grid=[
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]) == 3
+
+
+# 너비 우선 탐색 (BFS)
+def bfs_queue(graph, start):
+    visited = [start]
+    q = deque([start])
+
+    while q:
+        node = q.popleft()
+        for adj in graph[node]:
+            if adj not in visited:
+                q.append(adj)
+                visited.append(adj)
+
+    return visited
+
+
+# 섬의 개수 (BFS)
+def island_bfs(grid):
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+    rows, cols = len(grid), len(grid[0])
+    cnt = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] != '1':
+                continue
+
+            cnt += 1
+            q = deque([(row, col)])
+
+            while q:
+                x, y = q.popleft()
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if nx < 0 or nx >= rows or ny < 0 or ny >= cols or grid[nx][ny] != '1':
+                        continue
+                    grid[nx][ny] = '0'
+                    q.append((nx, ny))
+    return cnt
+
