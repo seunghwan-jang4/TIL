baa7793ddaec34c439ed7c9935a9508550e822c6
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-25 21:05:27 +0900

    add: 4주차, 버블정렬

---
+++ sparta_algorithm/4th_week/prac.py
@@ -0,0 +1,11 @@
+# 버블 정렬
+def bubblesort(lst):
+    # 최댓값을 구하는 알고리즘을 len(lst) - 1 만큼 반복한다.
+    iters = len(lst) - 1
+    for iter in range(iters):
+        # 이미 구한 최댓값은 범위에서 제외한다.
+        wall = iters - iter
+        for cur in range(wall):
+            if lst[cur] > lst[cur + 1]:
+                lst[cur], lst[cur + 1] = lst[cur + 1], lst[cur]
+    return lst
