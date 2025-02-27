c2b576fab2ae38beea00259201c1c1c92c9b3e8d
seunghwan-jang4 <200tmdghks@gmail.com>
2025-02-27 20:25:08 +0900

    update: 병합 정렬

--- sparta_algorithm/4th_week/prac.py
+++ sparta_algorithm/4th_week/prac.py
@@ -60,3 +60,26 @@ def quicksort(lst, start, end):
     quicksort(lst, start, p - 1)
     quicksort(lst, p + 1, end)
     return lst
+
+
+# 병합 정렬
+def merge(arr1, arr2):
+    result = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            result.append(arr1[i])
+            i += 1
+        else:
+            result.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        result.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        result.append(arr2[j])
+        j += 1
+
+    return result
--- sparta_algorithm/4th_week/test.py
+++ sparta_algorithm/4th_week/test.py
@@ -1,4 +1,4 @@
-from prac import bubblesort, selectionsort, insertionsort, quicksort
+from prac import bubblesort, selectionsort, insertionsort, quicksort, merge
 
 # 버블 정렬 테스트 코드
 assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
@@ -25,3 +25,7 @@ assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 3) == [1, 2, 3, 5, 3, 2, 3]
 assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 4) == [1, 2, 3, 3, 5, 2, 3]
 assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 5) == [1, 2, 2, 3, 3, 5, 3]
 assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 6) == [1, 2, 2, 3, 3, 3, 5]
+
+
+# 병합 정렬
+assert merge([1,2,3,5],[4,6,7,8]) == [1,2,3,4,5,6,7,8]
